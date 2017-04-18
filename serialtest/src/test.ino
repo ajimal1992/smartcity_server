0849c5c28eca8aaa15aae1952777d5b632a96037
Juan Jimeno <jimenojmm@gmail.com>
2017-04-18 15:52:02 +0800

    changed simulated data to dynamic

--- serialtest/src/test.ino
+++ serialtest/src/test.ino
@@ -1,14 +1,17 @@
-
+float temp = 24.04;
+float humidity = 67.46;
+int intensity = 1;
+int carpark = 1;
 
 void setup(){
     Serial.begin(115200);
 }
 
 void loop(){
-    float temp = 24.04;
-    float humidity = 67.46;
-    int intensity = 1;
-    int carpark = 1;
+     temp = temp + 0.1;
+     humidity = humidity + 0.1;
+     intensity = 1;
+     carpark = 1;
 
     Serial.print(temp);
     Serial.print('t');
